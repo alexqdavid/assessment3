a306d3728e3a0d730e190284ad836dd4ea83df8f
alexqdavid <alexqdavid@icloud.com>
2022-09-30 12:02:57 -0500

    test

---
+++ about.js
@@ -0,0 +1,28 @@
+console.log("hello world");
+
+
+
+function handleSubmit(evt) {
+	evt.preventDefault();
+	
+	//console.log('form submit');
+	alert('submitted successfully!!!')
+}
+function images() {
+	document.images[i].src.addEventListener('mouseover', images);
+}
+let form = document.querySelector('#contact');
+
+form.addEventListener('submit', handleSubmit);
+
+function places() {
+  alert("home");
+}
+
+function game() {
+	alert("gaming");
+  }
+
+  function colur() {
+	alert("blue");
+  }
---
+++ profile.js
@@ -0,0 +1,10 @@
+
+let button = document.querySelector("#color");
+
+function blue () {
+	document.body.style.backgroundColor = "blue";
+};
+
+button.addEventListener("click", blue);
+
+
